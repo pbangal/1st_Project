a9759923ea48b26cb7412c71fd3af2a4aef26551
Pratik <pratiquebangal@gmail.com>
2018-04-06 17:12:10 +0530

    Commited

---
+++ IfElse.py
@@ -0,0 +1,10 @@
+check = "Apple"
+if check == False:
+   print("It is false")
+elif check == "Banana":
+    print("Its tasty")
+elif check == "Apple":
+    print("Its red")
+
+else:
+    print("It is actually true")
