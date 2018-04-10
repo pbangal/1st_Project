1c01c9fa4f8b723b5ba3bd04a2e1af81d8628804
Pratik <pratiquebangal@gmail.com>
2018-04-10 18:11:34 +0530

    Commited

--- classes/game.py
+++ classes/game.py
@@ -24,3 +24,45 @@ class Person:
 
     def generate_damage(self):
         return random.randrange(self.atkl, self.atkh)
+
+    def generate_spell_damage(self, i):
+        ngl = self.magic[i]["dmg"] - 5
+        ngh = self.magic[i]["dmg"] + 5
+        return random.randrange(ngl, ngh)
+
+    def take_damage(self, dmg):
+        self.hp -= dmg
+        if self.hp <0:
+            self.hp =0
+            return self.hp
+
+    def get_hp(self):
+        return self.hp
+
+    def get_max_hp(self):
+        return self.maxhp
+
+    def get_mp(self):
+        return self.mp
+
+    def get_max_mp(self):
+        return self.maxmp
+
+    def reduce_mp(self, cost):
+        self.mp -= cost
+
+    def get_spell_name(self, i):
+        return self.magic[i]["name"]
+
+    def get_spell_mp_cost(self, i):
+        return self.magic[i]["cost"]
+
+    def choose_action(self):
+        i = 1
+        for item in self.actions:
+            print(str(i) + ":", item)
+            i += 1
+
+    def choose_magic(self):
+
+
